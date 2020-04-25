7ab9575d471d118fdf3274b7182a3891b49b50e9
chuta <chimeziechuta@yahoo.com>
2020-04-25 16:10:35 +0100

    initial comment

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         My awesome react app at Blockspace Technologies aka blockspace,africa. </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
